1157183aafce7932300625d6e97bfa3340495e4d
AlexBeattie42 <30098201+alexbeattie42@users.noreply.github.com>
2024-11-26 22:19:34 +0200

    Update to remove dead code

--- FloatingPointPrecision/main.cpp
+++ FloatingPointPrecision/main.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <cmath>
 #include <clocale>
-#include <chrono> // for std::chrono functions
+#include <chrono>
 
 int main()
 {
@@ -19,47 +19,34 @@ int main()
     const double last_num = 2510.65;
 
     const double offset = 10.675;
-    // // 40 FPS Sampling Rate
-    // const double rate = 1.0/40.0;
-    // // Last value expected in sampled array
-    // const double last_num = 2499.975;
 
-    // Create a vector of 1000 integers
     std::vector<int> integers(numEl);
     
-    // Fill the vector with values from 0 to 999
     for (int i = startVal; i < numEl; ++i) {
-        integers[i] = i; // Fill with values 0 to 999
+        integers[i] = i;
     }
 
-    // Create a vector to store the decimal values
     std::vector<double> decimalValues(numEl);
-
-    // Multiply each value by 0.01 and store in the decimalValues vector
 
     for (int i = startVal; i < numEl; ++i) {
         decimalValues[i] = integers[i] * rate + offset;
     }
 
-    // Create a vector to store the decimal values
     std::vector<double> decimalValues2(numEl);
-    // Start from 0.0 and add 0.01 to each subsequent element
-    decimalValues2[0] = offset; // Initialize the first element to 0.0
+
+    decimalValues2[0] = offset;
+    double time = decimalValues2[0];
     for (int i = 1; i < numEl; ++i) {
-        decimalValues2[i] = decimalValues2[i - 1] + rate; // Each element is previous element + 0.01
+        time += rate;
+        decimalValues2[i] = time;
     }
 
-    // Create a vector to store the decimal values
     std::vector<double> decimalValues3(numEl);
-
-    // Multiply each value by 0.01 and store in the decimalValues vector
 
     for (int i = startVal; i < numEl; ++i) {
         decimalValues3[i] = std::fma(integers[i],rate, offset);
     }
 
-
-    // Print the decimal values to 8 decimal places
     for (int i = startVal; i < numEl; ++i) {
         std::cout << std::fixed << std::setprecision(32) << "Add: " << decimalValues2[i]  << " Multiply: " << decimalValues[i] << " FMA: " << decimalValues3[i] << std::endl;
     }
