3ca6a7766067e33b8fcbe71fcb2f6b08d915b2af
AlexBeattie42 <30098201+alexbeattie42@users.noreply.github.com>
2024-11-26 17:55:38 +0200

    Update uniform spacing example for std::fma

--- FloatingPointPrecision/main.cpp
+++ FloatingPointPrecision/main.cpp
@@ -13,10 +13,12 @@ int main()
     const int startVal = 0;
     const int numEl = 100000;
 
-    // 100 FPS Sampling Rate
-    const double rate = 1.0/100.0;
+    // 40 FPS Sampling Rate
+    const double rate = 1.0/40.0;
     // Last value expected in sampled array
-    const double last_num = 999.99;
+    const double last_num = 2510.175;
+
+    const double offset = 10.2;
     // // 40 FPS Sampling Rate
     // const double rate = 1.0/40.0;
     // // Last value expected in sampled array
@@ -36,27 +38,38 @@ int main()
     // Multiply each value by 0.01 and store in the decimalValues vector
 
     for (int i = startVal; i < numEl; ++i) {
-        decimalValues[i] = integers[i] * rate;
+        decimalValues[i] = integers[i] * rate + offset;
     }
 
     // Create a vector to store the decimal values
     std::vector<double> decimalValues2(numEl);
     // Start from 0.0 and add 0.01 to each subsequent element
-    decimalValues[0] = 0.0; // Initialize the first element to 0.0
+    decimalValues2[0] = offset; // Initialize the first element to 0.0
     for (int i = 1; i < numEl; ++i) {
         decimalValues2[i] = decimalValues2[i - 1] + rate; // Each element is previous element + 0.01
     }
 
+    // Create a vector to store the decimal values
+    std::vector<double> decimalValues3(numEl);
+
+    // Multiply each value by 0.01 and store in the decimalValues vector
+
+    for (int i = startVal; i < numEl; ++i) {
+        decimalValues3[i] = std::fma(integers[i],rate, offset);
+    }
+
+
     // Print the decimal values to 8 decimal places
     for (int i = startVal; i < numEl; ++i) {
-        std::cout << std::fixed << std::setprecision(16) <<"Multiply: " << decimalValues[i] << " Add: " << decimalValues2[i] << std::endl;
+        std::cout << std::fixed << std::setprecision(32) <<"Multiply: " << decimalValues[i] << " Add: " << decimalValues2[i] << " FMA: " << decimalValues3[i] << std::endl;
     }
 
     const double err1 = std::abs(last_num - decimalValues[numEl - 1]);
     const double err2 = std::abs(last_num - decimalValues2[numEl - 1]);
+    const double err3 = std::abs(last_num - decimalValues3[numEl - 1]);
 
     std::cout << "\nError of last value: " << std::endl;
-    std::cout << std::fixed << std::setprecision(24) << "Multiplication: " << err1 << " Addition: " << err2 << std::endl;
+    std::cout << std::fixed << std::setprecision(32) << "Multiplication: " << err1 << " Addition: " << err2 << " FMA: " << err3 <<std::endl;
    
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     std::cout << "Runtime = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
