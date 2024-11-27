0ddcba7fbe3e4a6789ffcc85c0a3eadb025252fa
AlexBeattie42 <30098201+alexbeattie42@users.noreply.github.com>
2024-11-27 16:24:24 +0200

    Add Uniform Sampling of time custom function

--- FloatingPointPrecision/main.cpp
+++ FloatingPointPrecision/main.cpp
@@ -1,10 +1,24 @@
+#include <algorithm>
 #include <chrono>
 #include <clocale>
 #include <cmath>
 #include <iomanip>
 #include <iostream>
+#include <numeric>
 #include <string>
 #include <vector>
+
+template <typename T>
+std::vector<T> uniformTimeSamples(const T offset, const T step_size, const int numEl) {
+    std::vector<int> ivec(numEl);
+    std::iota(ivec.begin(), ivec.end(), 0); // ivec will become: [0..99]
+    std::vector<T> output(ivec.size());
+    std::transform(ivec.begin(), ivec.end(), output.begin(),
+                   [step_size, offset](int value) {
+                       return std::fma(value, step_size, offset);
+                   });
+    return output;
+}
 
 int main() {
   std::chrono::steady_clock::time_point begin =
@@ -44,20 +58,24 @@ int main() {
     decimalValues3[i] = std::fma(integers[i], rate, offset);
   }
 
+  std::vector<double> decimalValues4 = uniformTimeSamples(offset, rate, numEl);
+
   for (int i = startVal; i < numEl; ++i) {
     std::cout << std::fixed << std::setprecision(32)
               << "Add: " << decimalValues[i]
               << " Multiply: " << decimalValues2[i]
-              << " FMA: " << decimalValues3[i] << std::endl;
+              << " FMA: " << decimalValues3[i]
+              << " CUSTOM: " << decimalValues4[i] << std::endl;
   }
 
   const double err1 = std::abs(last_num - decimalValues[numEl - 1]);
   const double err2 = std::abs(last_num - decimalValues2[numEl - 1]);
   const double err3 = std::abs(last_num - decimalValues3[numEl - 1]);
+  const double err4 = std::abs(last_num - decimalValues3[numEl - 1]);
 
   std::cout << "\nError of last value: " << std::endl;
   std::cout << std::fixed << std::setprecision(32) << "Add: " << err1
-            << " Multiply: " << err2 << " FMA: " << err3 << std::endl;
+            << " Multiply: " << err2 << " FMA: " << err3 << " CUSTOM: " << err4 << std::endl;
 
   std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
   std::cout << "Runtime = "
